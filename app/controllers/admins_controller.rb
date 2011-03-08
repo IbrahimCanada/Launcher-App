b2818367fbe84194433c7198a1e594ca1ebf9ad4
Jay Wengrow <jaywngrw@gmail.com>
2011-03-08 15:58:58 -0600

    some minor edits

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,7 +10,8 @@ class AdminsController < ApplicationController
   		sign_in @admin
   		redirect_to admin_path(@admin)
   	else
-  		flash[:notice] = "Account not created!"
+  		#flash[:notice] = "Account not created!"
+  		flash[:error] = "Sign up not successful. " + @admin.errors.full_messages[0]
 			redirect_to root_path
   	end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 		 		@referrer.increment_signups
 			end
 			if @user.save
-				flash[:success] = "You're all signed up! You're link is http://getyourlaunchbox.com/" + @user.link  		
+				flash[:success] = "Thanks for signing up!" 	
 			else
 				flash[:error] = "Sign up not successful. " + @user.errors.full_messages[0]
 			end
