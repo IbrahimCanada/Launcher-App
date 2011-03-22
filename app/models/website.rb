7697e65fa53f36243862ccb1c4616a66afa9d604
Jay Wengrow <jaywngrw@gmail.com>
2011-03-22 12:13:27 -0500

    completed s3 functionality

--- app/models/website.rb
+++ app/models/website.rb
@@ -26,9 +26,8 @@ class Website < ActiveRecord::Base
 	has_many :users
 	belongs_to :admin
 	
-	has_attached_file :logo, :storage => :s3, :bucket => 'mybucket', :s3_credentials => {:access_key_id => ENV['AKIAIPF5UI3OMTUMT7DQ'], 
-	:secret_access_key => ENV['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']
-    }#, :path => ":attachment/:id/:style.:extension" #, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :logo, :storage => :s3, :s3_credentials => { :access_key_id => 'AKIAIPF5UI3OMTUMT7DQ', 
+	:secret_access_key => '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', :bucket => 'launchbox' }, :path => ":attachment/:id/:style.:extension" #, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
 	
 	
