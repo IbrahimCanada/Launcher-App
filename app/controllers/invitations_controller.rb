d4776836465cb21c60a80ee50ed6542e5940bf1c
Jay Wengrow <jaywngrw@gmail.com>
2011-03-30 12:34:21 -0500

    can send multiple emails at once separated by commas

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,12 +1,15 @@
 class InvitationsController < ApplicationController
 
 	def invite
-		@friend_email = params[:friend_email]
+		@friend_emails = params[:friend_email].split(',')
 		@user = User.find(params[:user])
 		@website = Website.find(params[:website])
-		flash[:notice] = "Your friend has been invited! #{@friend_email}"
-		FriendMailer.invite_email(@friend_email, @user.email, @user.link, @website.url).deliver
-		redirect_to root_path
+		flash[:notice] = "Your friends have been invited!"
+		@friend_emails.each do |friend_email|
+			FriendMailer.invite_email(friend_email, @user.email, @user.link, @website.url).deliver
+		end
+		flash[:user_show] = @user.id
+		redirect_to website_path(@website)
 	end
 	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def create
 	  @website = Website.find(params[:user][:website])
-	  if User.find_by_email(params[:user][:email]) && User.find_by_email(params[:user][:email]).website == @website
-	  	@user = User.find_by_email(params[:user][:email])
+	  if User.find_by_email_and_website_id(params[:user][:email], @website) #User.find_by_email(params[:user][:email]) && User.find_by_email(params[:user][:email]).website == @website
+	  	@user = User.find_by_email_and_website_id(params[:user][:email], @website)
 	  else
 			@user = @website.users.create( :email => params[:user][:email] ) 	
 			if @user.save
